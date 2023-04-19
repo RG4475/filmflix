389dac2f6038b0d91a6f78f0d65781716b75c0ea
Robert <rgrady771@hotmail.com>
2023-04-19 01:01:41 +0100

    Created printReports function in reports.py where all film reports are printed from to reduce code repetition

--- reports.py
+++ reports.py
@@ -15,7 +15,15 @@ def readReports():
         
     return readoption
 
-readprogram = False
+def printReports(films, noReturnMsg):
+
+    if not films:
+        print(f"No films have been returned {noReturnMsg}")
+    else:
+        for returned in films:
+            print(returned)
+
+readprogram = True
 
 while readprogram:
     selected = readReports()
@@ -28,35 +36,28 @@ while readprogram:
         cursor.execute(f"SELECT title, yearReleased, rating, duration FROM tblFilms WHERE genre = \'{genre}\'")
 
         filmsReturned = cursor.fetchall()
-        # noGenre = "that are of that particular genre"
+        noGenre = "that are of that particular genre"
 
-        for returned in filmsReturned:
-            print(returned)
+        printReports(filmsReturned, noGenre)
 
     elif selected == "3":
         year = int(input("Enter the year to see all films that were released in that year "))
         cursor.execute(f"SELECT title, rating, duration, genre FROM tblFilms WHERE yearReleased = {year}")
 
         filmsReturned = cursor.fetchall()
-        # noYear = "that were released in that year"
+        noYear = "that were released in that year"
 
-        for returned in filmsReturned:
-            print(returned)
+        printReports(filmsReturned, noYear)
 
     elif selected == "4":
-        rating = input("Enter the suitability rating to see all films that have that suitability rating").upper()
+        rating = input("Enter the suitability rating to see all films that have that suitability rating ").upper()
         cursor.execute(f"SELECT title, yearReleased, duration, genre FROM tblFilms WHERE rating = \'{rating}\'")
 
         filmsReturned = cursor.fetchall()
-        # noRating = "That have that suitability rating"
+        noRating = "that have that suitability rating"
 
-        for returned in filmsReturned:
-            print(returned)
+        printReports(filmsReturned, noRating)
     
     else:
         readprogram = False
-""" 
-def printReports(films, noReturnMsg):
 
-    if not films:
-        print(f"No films have been returned {noReturnMsg}") """
